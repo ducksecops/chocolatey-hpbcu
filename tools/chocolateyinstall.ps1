cc36096dc8d2d3987d33bf8749826a5030464099
ducksecops <daniel@ducksecops.uk>
2020-01-20 12:05:30 +0000

    Initial chocolatey package for 4.0.30.1

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://ftp.hp.com/pub/softpaq/sp100501-101000/sp100599.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+  url           = $url
+ 
+
+  softwareName  = 'HP BIOS Config Utility*'
+
+  checksum      = '3B38FABC8F27E85687E2479DCEC9AF22C069EAF88C701E4D4811433003CD038D'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/s'
+}
+
+Install-ChocolateyPackage @packageArgs
